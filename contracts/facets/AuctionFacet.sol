7f82b59083ebd556032211761da350ab6f332c77
signor1 <emmanuelomemgboji@gmail.com>
2024-03-24 07:42:38 +0100

    feat: finished up with auction facet

--- contracts/facets/AuctionFacet.sol
+++ contracts/facets/AuctionFacet.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 
 import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import {LibAppStorage} from "../libraries/LibAppStorage.sol";
+import {LibAppConstant} from "../libraries/LipAppConstant.sol";
 
 error WRONG_DURATION_ENTERED();
 error WRONG_PRICE_ENTERED();
@@ -11,6 +12,9 @@ error AUCTION_TIME_HAS_ELASPED();
 error AUCTION_BY_INDEX_DOES_NOT_EXIST();
 error YOUR_BID_IS_LESS_THAN_THE_STARTING_BID();
 error TOKEN_BALANCE_IS_NOT_ENOUGH();
+error YOUR_BID_IS_NOT_ENOUGH();
+error AUCTION_HAS_NOT_ENDED();
+error YOU_ARE_NOT_THE_HIGHEST_BIDDER();
 
 contract AuctionFacet {
     LibAppStorage.Layout internal l;
@@ -20,6 +24,32 @@ contract AuctionFacet {
         address indexed itemLister,
         uint256 tokenId,
         uint256 auctionPrice
+    );
+
+    event PreviousBidderPaid(
+        address indexed previousBidder,
+        uint256 indexed amount
+    );
+
+    event PercentageBurn(address from, address to, uint256 amount);
+    event DAOPercentage(address from, address to, uint256 amount);
+    event TeamWalletPercentage(address from, address to, uint256 amount);
+    event LastInteractorPercentage(address from, address to, uint256 amount);
+    event HighestBidderUpdated(
+        address indexed newHighestBidder,
+        uint256 amount,
+        uint256 time
+    );
+    event AuctionEnded(uint256 auctionId);
+    event AuctionItemCollected(
+        uint256 auctionId,
+        address collector,
+        uint256 tokenId
+    );
+    event AuctionItemRetrievedBack(
+        uint256 auctionId,
+        address creator,
+        uint256 tokenId
     );
 
     function createAuction(
@@ -49,6 +79,7 @@ contract AuctionFacet {
         a.duration = _duration;
         a.startingBid = _startingBid;
         a.nftTokenId = _tokenId;
+        a.auctionCreator = msg.sender;
 
         l.auctionIndex = l.auctionIndex + 1;
 
@@ -72,6 +103,8 @@ contract AuctionFacet {
 
         //checking if the duration has elasped
         if (block.timestamp > a.duration) {
+            a.hasEnded = true;
+            emit AuctionEnded(a.auctionId);
             revert AUCTION_TIME_HAS_ELASPED();
         }
 
@@ -83,6 +116,162 @@ contract AuctionFacet {
 
         if (balance < _amount) {
             revert TOKEN_BALANCE_IS_NOT_ENOUGH();
+        }
+
+        //transfering the token from the msg.sender to the address(this)
+        LipAppStorage._transferFrom(msg.sender, address(this), _amount);
+
+        if (a.currentBid == 0) {
+            a.highestBidder = msg.sender;
+            a.currentBid = _amount;
+
+            emit HighestBidderUpdated(
+                a.highestBidder,
+                a.currentBid,
+                block.timestamp
+            );
+        } else {
+            uint check = (a.currentBid * 20) / 100;
+            uint estimate = a.currentBid + check;
+            if (_amount < estimate) {
+                revert YOUR_BID_IS_NOT_ENOUGH();
+            }
+
+            a.previousBidder = a.highestBidder;
+
+            // calculations
+            //paying back previous bidder
+            payPreviousBidder(_amount);
+            //percentage distribution
+            noLossDistribution(_amount);
+
+            a.currentBid = _amount;
+            a.highestBidder = msg.sender;
+
+            emit HighestBidderUpdated(
+                a.highestBidder,
+                a.currentBid,
+                block.timestamp
+            );
+        }
+    }
+
+    //method to pay back previous bidder
+    function payPreviousBidder(uint256 _amount) private {
+        uint256 nextBidderPercent = (_amount * LibAppConstant.PREVIOUS_BIDDER) /
+            100;
+
+        uint256 accruedAmount = nextBidderPercent + a.currentBid;
+
+        //send back previous bidder's amount
+        LibAppStorage._transferFrom(
+            address(this),
+            a.previousBidder,
+            a.currentBid
+        );
+
+        emit PreviousBidderPaid(a.previousBidder, accruedAmount);
+    }
+
+    //method to calculate percentage distribution
+    function noLossDistribution(uint256 _amount) private {
+        // Handle Burn
+        uint256 burnableAmount = (_amount * LibAppConstant.BURNABLE) / 100;
+        LibAppStorage._transferFrom(address(this), address(0), burnableAmount);
+        emit PercentageBurn(address(this), address(0), burnableAmount);
+
+        //handle DAO
+        uint256 daoAmount = (_amount * LibAppConstant.DAO) / 100;
+        LibAppStorage._transferFrom(
+            address(this),
+            LibAppConstant.DAO_WALLET,
+            daoAmount
+        );
+        emit DAOPercentage(address(this), LibAppConstant.DAO_WALLET, daoAmount);
+
+        // Handle team fees
+        uint256 teamAmount = (_amount * LibAppConstant.TEAM_WALLET) / 100;
+        LibAppStorage._transferFrom(
+            address(this),
+            LibAppConstant.TEAM_WALLET_ADDRESS,
+            teamAmount
+        );
+        emit TeamWalletPercentage(
+            address(this),
+            LibAppConstant.TEAM_WALLET_ADDRESS,
+            teamAmount
+        );
+
+        //Last interactor
+        uint256 lastInteractorAmount = (_amount *
+            LibAppConstant.LAST_INTERACTOR) / 100;
+
+        LibAppStorage._transferFrom(
+            address(this),
+            msg.sender,
+            lastInteractorAmount
+        );
+        emit LastInteractorPercentage(
+            address(this),
+            msg.sender,
+            lastInteractorAmount
+        );
+    }
+
+    //collecting the auctioned item when the highest bidder
+    function collectAuctionItem(uint8 _auctionId) external {
+        //checking auction by index exist
+        if (!l.auctions[_auctionId]) {
+            revert AUCTION_BY_INDEX_DOES_NOT_EXIST();
+        }
+        if (!l.auctions[_auctionId].hasEnded) {
+            revert AUCTION_HAS_NOT_ENDED();
+        }
+        if (l.auctions[_auctionId].highestBidder != msg.sender) {
+            revert YOU_ARE_NOT_THE_HIGHEST_BIDDER();
+        }
+
+        //transfering the token from the address(this) to highest bidder
+        IERC721(l.nftContractAddress).safeTransferFrom(
+            address(this),
+            l.auctions[_auctionId].highestBidder,
+            l.auctions[_auctionId].nftTokenId
+        );
+
+        l.auctions[_auctionId].hasEnded = true;
+
+        emit AuctionItemCollected(
+            _auctionId,
+            msg.sender,
+            l.auctions[_auctionId].nftTokenId
+        );
+    }
+
+    //collecting the auctioned item if no bidder
+    function collectAuctionedItemIfNoBidder(uint8 _auctionId) external {
+        //checking auction by index exist
+        if (!l.auctions[_auctionId]) {
+            revert AUCTION_BY_INDEX_DOES_NOT_EXIST();
+        }
+        if (block.timestamp > l.auctions[_auctionId].duration) {
+            if (l.auctions[_auctionId].currentBid == 0) {
+                //transfering the token from the address(this) to highest bidder
+                IERC721(l.nftContractAddress).safeTransferFrom(
+                    address(this),
+                    l.auctions[_auctionId].auctionCreator,
+                    l.auctions[_auctionId].nftTokenId
+                );
+
+                l.auctions[_auctionId].hasEnded = true;
+
+                emit AuctionItemRetrievedBack(
+                    l.auctions[_auctionId].auctionId,
+                    l.auctions[_auctionId].auctionCreator,
+                    l.auctions[_auctionId].nftTokenId
+                );
+            }
+        } else {
+            revert AUCTION_HAS_NOT_ENDED();
         }
     }
 }
--- contracts/libraries/LibAppStorage.sol
+++ contracts/libraries/LibAppStorage.sol
@@ -1,6 +1,10 @@
 pragma solidity ^0.8.0;
 
+error NOT_ENOUGH_TOKEN_TO_TRANSFER();
+
 library LibAppStorage {
+    event Transfer(address indexed _from, address indexed _to, uint256 _amount);
+
     struct AuctionDetail {
         uint8 auctionId;
         uint256 duration;
@@ -8,6 +12,7 @@ library LibAppStorage {
         uint256 currentBid;
         uint256 nftTokenId;
         bool hasEnded;
+        address auctionCreator;
         address highestBidder;
         address previousBidder;
     }
@@ -24,5 +29,27 @@ library LibAppStorage {
         uint8 auctionIndex;
         address nftContractAddress;
         mapping(uint8 => AuctionDetail) auctions;
+    }
+
+    function layoutStorage() internal pure returns (Layout storage l) {
+        assembly {
+            l.slot := 0
+        }
+    }
+
+    function _transferFrom(
+        address _from,
+        address _to,
+        uint256 _amount
+    ) internal {
+        Layout storage l = layoutStorage();
+        uint256 balance = l.balances[msg.sender];
+
+        if (balance < _amount) {
+            revert NOT_ENOUGH_TOKEN_TO_TRANSFER();
+        }
+        l.balances[_from] = balance - _amount;
+        l.balances[_to] += _amount;
+        emit Transfer(_from, _to, _amount);
     }
 }
